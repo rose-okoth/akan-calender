e8cfec135c8cdd73b44a9a4d72ccfd5db594a7cd
rose-okoth <okoth.rose0@gmail.com>
2020-12-12 19:09:25 +0300

    add the changes to the javascript file.

---
+++ js/scripts.js
@@ -0,0 +1,39 @@
+Day of the week (d) = ( ( (century/4) -2*century-1) + ((5*year/4) ) + ((26*(month+1)/10)) + date ) % 7;
+
+var days = ['Sunday','Monday','Tuesday','Wednesday','Thursday','Friday','Saturday'];
+
+var months = ['January','February','March','April','May','June','July','August','September','October','November','December'];
+
+if (d = 0 && male) {
+    console.log("Your Akan name is Kwasi.");
+} else if (d = 1 && male) {
+    console.log("Your Akan name is Kwadwo.");
+} else if (d = 2 && male) {
+    console.log("Your Akan name is Kwabena.");
+} else if (d = 3 && male) {
+    console.log("Your Akan name is Kwaku.");
+} else if (d = 4 && male) {
+    console.log("Your Akan name is Yaw.");
+} else if (d = 5 && male) {
+    console.log("Your Akan name is Kofi.");
+} else if (d = 6 && male) {
+    console.log("Your Akan name is Kwame.");
+} else if (d = 0 && female) {
+    console.log("Your Akan name is Akosua.");
+} else if (d = 1 && female) {
+    console.log("Your Akan name is Adwoa.");
+} else if (d = 2 && female) {
+    console.log("Your Akan name is Abenaa.");
+} else if (d = 3 && female) {
+    console.log("Your Akan name is Akua.");
+} else if (d = 4 && female) {
+    console.log("Your Akan name is Yaa.");
+} else if (d = 5 && female) {
+    console.log("Your Akan name is Afua.");
+} else if (d= 6 && female) {
+    console.log("Your Akan name is Ama.");
+} else if (date<=0 || date>31) {
+    console.log("Invalid date. Input correct date.")
+} else if (month<= || month>12) {
+    console.log("Invalid month. input the correct month.")
+}
