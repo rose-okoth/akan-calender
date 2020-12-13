28defa583ec97d790d802101adde8eebaeb1ec7b
rose-okoth <okoth.rose0@gmail.com>
2020-12-13 20:10:11 +0300

    add the changes to the index.html.

--- js/scripts.js
+++ js/scripts.js
@@ -1,5 +1,3 @@
-var d = new Date();
-document.getElementById("demo").innerHTML = d.getDay();
 function zeller(month, day, year) {
     if (month < 3) {
        month += 12; year -= 1;
@@ -12,10 +10,13 @@ function zeller(month, day, year) {
 
  function demo(month, day, year) {
     var mons =
-        ['January','February','March','April','May','June','July','August','September','October','November','December'];
+        ['January','February','March','April','May','June',
+        'July','August','September','October','November','December'];
     var days =
-       ['Sunday', 'Monday', 'Tuesday', 'Wednesday','Thursday', 'Friday', 'Saturday'];
-    return mons[month-1] + ' ' + day + ', ' + year +' = ' + days[zeller(month, day, year)];
+       ['Sunday', 'Monday', 'Tuesday', 'Wednesday',
+       'Thursday', 'Friday', 'Saturday'];
+    return mons[month-1] + ' ' + day + ', ' + year +
+    ' = ' + days[zeller(month, day, year)];
  }
  
  function doDemo() {
@@ -25,36 +26,4 @@ function zeller(month, day, year) {
     document.getElementById('result').innerHTML = demo(m,d,y);
  }
 
-if (d = 0 && male) {
-    console.log("Your Akan name is Kwasi.");
-} else if (d = 1 && male) {
-    console.log("Your Akan name is Kwadwo.");
-} else if (d = 2 && male) {
-    console.log("Your Akan name is Kwabena.");
-} else if (d = 3 && male) {
-    console.log("Your Akan name is Kwaku.");
-} else if (d = 4 && male) {
-    console.log("Your Akan name is Yaw.");
-} else if (d = 5 && male) {
-    console.log("Your Akan name is Kofi.");
-} else if (d = 6 && male) {
-    console.log("Your Akan name is Kwame.");
-} else if (d = 0 && female) {
-    console.log("Your Akan name is Akosua.");
-} else if (d = 1 && female) {
-    console.log("Your Akan name is Adwoa.");
-} else if (d = 2 && female) {
-    console.log("Your Akan name is Abenaa.");
-} else if (d = 3 && female) {
-    console.log("Your Akan name is Akua.");
-} else if (d = 4 && female) {
-    console.log("Your Akan name is Yaa.");
-} else if (d = 5 && female) {
-    console.log("Your Akan name is Afua.");
-} else if (d= 6 && female) {
-    console.log("Your Akan name is Ama.");
-} else if (date<=0 || date>31) {
-    console.log("Invalid date. Input correct date.")
-} else if (month<= || month>12) {
-    console.log("Invalid month. input the correct month.")
-}
+
