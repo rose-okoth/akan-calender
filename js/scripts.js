659ff54d1286424b3fb4e04b635f6e8b7cd1ef2f
rose-okoth <okoth.rose0@gmail.com>
2020-12-13 20:29:38 +0300

    add the modifications to the index.html

--- js/scripts.js
+++ js/scripts.js
@@ -26,4 +26,36 @@ function zeller(month, day, year) {
     document.getElementById('result').innerHTML = demo(m,d,y);
  }
 
-
+ if (day= 0 && male) {
+   console.log("Your Akan name is Kwasi.");
+} else if (days = 1 && male) {
+   console.log("Your Akan name is Kwadwo.");
+} else if (days = 2 && male) {
+   console.log("Your Akan name is Kwabena.");
+} else if (days = 3 && male) {
+   console.log("Your Akan name is Kwaku.");
+} else if (days = 4 && male) {
+   console.log("Your Akan name is Yaw.");
+} else if (days = 5 && male) {
+   console.log("Your Akan name is Kofi.");
+} else if (days = 6 && male) {
+   console.log("Your Akan name is Kwame.");
+} else if (days = 0 && female) {
+   console.log("Your Akan name is Akosua.");
+} else if (days = 1 && female) {
+   console.log("Your Akan name is Adwoa.");
+} else if (days = 2 && female) {
+   console.log("Your Akan name is Abenaa.");
+} else if (days = 3 && female) {
+   console.log("Your Akan name is Akua.");
+} else if (days = 4 && female) {
+   console.log("Your Akan name is Yaa.");
+} else if (days = 5 && female) {
+   console.log("Your Akan name is Afua.");
+} else if (days = 6 && female) {
+   console.log("Your Akan name is Ama.");
+} else if (date<=0 || date>31) {
+   console.log("Invalid date. Input correct date.")
+} else if (month<=0 || month>12) {
+   console.log("Invalid month. input the correct month.")
+}
